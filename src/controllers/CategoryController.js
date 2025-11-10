88342a2ee806ad07a16e27ff90aaf6a80d417bda
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2025-11-10 11:31:54 +0800

    Added pagination and filter function on category and added fixes of product

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,8 +1,36 @@
 import * as categoryModel from "../models/CategoryModel.js";
 
 export const fetchCategory = async(req, res) =>{
-    const categories = await categoryModel.getCategory();
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const search = req.query.search || '';
+
+    const categories = await categoryModel.getCategory(page, limit, search);
     res.status(200).json({success: true, message: categories});
+}
+
+export const fetchCategoryCount = async (req, res) =>{
+    const search = req.query.search || '';
+
+    const total_categories = await categoryModel.getCategoryCount(search);
+    res.status(200).json({success: true, message: [total_categories]})
+}
+
+export const fetchCategoryById = async(req, res, next) =>{
+    const id = req.params.id || -1;
+
+    try{
+        const category = await categoryModel.getCategoryById(id);
+        res.status(200).json({
+            success: true,
+            message: [
+                category
+            ]
+        })
+    }catch(err){
+        console.log(err);
+        next(err);
+    }
 }
 
 export const createCategory = async(req, res, next) => {
--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -1,8 +1,45 @@
 import pool from "../config/db.js";
 
-export const getCategory = async() =>{
-    const [rows] = await pool.query("SELECT * FROM category");
+export const getCategory = async(page = 1, limit = 10, search = '') =>{
+    const offset = (page - 1) * limit;
+
+    let query = "SELECT * FROM category WHERE 1=1 ";
+    const params = [];
+
+    if(search){
+        query += "AND category_name LIKE ? ";
+        params.push(`%${search}%`);
+    }
+
+    query += "LIMIT ? OFFSET ?";
+    params.push(limit, offset);
+
+    const [rows] = await pool.query(query, params);
     return rows;
+}
+
+export const getCategoryCount = async (search='') =>{
+    let query = "SELECT COUNT(*) AS total FROM category WHERE 1=1 ";
+    const params = [];
+
+    if(search){
+        query += "AND name LIKE ? ";
+        params.push(`%${search}%`);
+    }
+
+    const [[{total}]] = await pool.query(query, params);
+    return total;
+}
+
+export const getCategoryById = async (id = -1) =>{
+    if(id === -1 || id == NaN){
+        const error = new Error('Invalid id');
+        error.statusCode = 400;
+        throw error;
+    }
+
+    const [rows] = await pool.query("SELECT * FROM category WHERE id = ?", [id]);
+    return rows[0];
 }
 
 export const insertCategory = async(name, description) =>{
